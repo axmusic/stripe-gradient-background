cde6f42f462d34fab7fcc7cf3605f3dedf57d820
axmusic <adarshghagre@gmail.com>
2025-07-28 21:13:28 +0530

    Minification added

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
-const mix = require( 'laravel-mix' );
+const mix = require('laravel-mix');
 
-mix.setPublicPath( './dist' );
-mix.js( 'src/Gradient.js', 'stripe-gradient.js' );
+mix.setPublicPath('./dist');
+mix.js('src/Gradient.js', 'stripe-gradient.js');
+mix.minify('dist/stripe-gradient.js');
